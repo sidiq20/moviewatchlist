1a140d353a6b5ef0a96e85fae239b39d8d3e7ae2
sidiq20 <sidiqolasode@gmail.com>
2025-02-03 20:54:08 +0100

    init

---
+++ __init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+from routes import pages
+
+load_dotenv()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config["MONGODB_URI"] = os.environ.get("MONGODB_URI")
+    app.config["SECRET_KEY"] = os.environ.get(
+        "SECRET_KEY", "20bc721c9186545533e0db2372305f40"
+    )
+
+    client = MongoClient(app.config["MONGODB_URI"])
+
+    app.db = client["moviewatchlist"]
+
+    app.register_blueprint(pages)
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True, port=7000)
