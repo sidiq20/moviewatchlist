aafcd0d5f19ae66d84456eb3da1ae1b4c608dddb
sidiq20 <sidiqolasode@gmail.com>
2025-02-13 18:22:51 +0100

    new commit

--- forms.py
+++ forms.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, SubmitField
-
+from wtforms import IntegerField, StringField, SubmitField, TextAreaField
 from wtforms.validators import InputRequired, NumberRange
 
 
