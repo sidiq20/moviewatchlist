b980d8661b7470fc700d22de5d875348c21a548d
sidiq20 <sidiqolasode@gmail.com>
2025-02-16 20:54:56 +0100

    added a new class register

--- forms.py
+++ forms.py
@@ -39,3 +39,27 @@ class ExtendedMovieForm(MovieForm):
     video_link = URLField("Video Link")
 
     submit = SubmitField("Submit")
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[InputRequired(), Email()])
+    password = PasswordField(
+        "Password",
+                 validators=[
+                     InputRequired(),
+                     Length(
+                         min=4,
+                         max=20,
+                         message="Your password must be between 4 and 20 characters long"
+                     )
+                 ])
+    confirm_password = PasswordField(
+        "Confirm Password",
+        validators=[
+            InputRequired(),
+            EqualTo(
+                "password",
+                message="This password did not match the one in the password field"
+            )
+        ]
+    )
+    submit = SubmitField("Register")
