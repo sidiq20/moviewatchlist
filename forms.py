18916d7543dcc1d7168567c59d6ae0f6d47617fe
sidiq20 <sidiqolasode@gmail.com>
2025-02-16 21:04:09 +0100

    added a new class LoginForm

--- forms.py
+++ forms.py
@@ -63,3 +63,8 @@ class RegisterForm(FlaskForm):
         ]
     )
     submit = SubmitField("Register")
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[InputRequired(), Email()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    submit = SubmitField("Login")
